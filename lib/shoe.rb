b86ff4d6d848c7f2a976a769c1d6fa6ae53f7d89
james <jayrh3246@gmail.com>
2021-02-23 15:09:44 -0500

    commit tree 20a5bb5e0551b8654e180d511a9ffe147de57cb5

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,36 @@
-# Make your shoe class here!
+class Shoe
+    def initialize(brand)
+        @brand = brand
+    end
+    def brand
+        @brand
+    end
+    def color=(color)
+        @color = color
+    end
+    def color
+        @color
+    end
+    def size=(size)
+        @size = size
+    end
+    def size
+        @size
+    end
+    def material=(some_material)
+        @material = some_material
+    end
+    def material
+        @material
+    end
+    def condition=(some_condition)
+        @condition = some_condition
+    end
+    def condition
+        @condition
+    end
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end  
+end
